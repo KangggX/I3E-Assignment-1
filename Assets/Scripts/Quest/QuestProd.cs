89a6d2081c4fc712808eff02a3445cbeca77b577
KangggX <lowkangxuan@gmail.com>
2021-07-16 23:13:49 +0800

    edited quest script

---
+++ Assets/Scripts/Quest/QuestProd.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestProd : MonoBehaviour
+{
+    public List<string> questList = new List<string>();
+}
